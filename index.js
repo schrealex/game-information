c6217f21301a3791d92db6de3eb34dbadec5fee3
Ceryl Bruitzman <ceryl.bruitzman@topicus.nl>
2022-10-03 12:08:24 +0200

    Several changes: - Removed HLTB dependency, no longer used. - Implemented new request, with using the howlongtobeat API directly.

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const https = require('https');
 const cors = require('cors');
-let hltb = require('howlongtobeat');
+const fetch = require('node-fetch');
 
 const app = express();
 const port = 3000;
 
-let hltbService = new hltb.HowLongToBeatService();
+// let hltbService = new hltb.HowLongToBeatService();
 
 app.use(cors());
 
@@ -18,10 +17,57 @@ app.listen(port, () => {
   console.log(`Server running on ${port}, http://localhost:${port}`);
 });
 
-app.get('/get-game-info', (request, response) => {
-  const gameTitle = request.query.title;
+app.get('/how-long-to-beat', (request, response) => {
+  const searchTerm = request.query.title;
 
-  hltbService.search(gameTitle).then(result => {
+  getHLTBInformation(searchTerm).then(result => {
     response.status(200).send(result);
+  }).catch((error) => {
+    if (error.name === 'FetchError') {
+      response.status(400).send('Title query wasn\'t given with request');
+    } else {
+      response.status(500).send(error);
+    }
   });
 });
+
+const getHLTBInformation = async (searchTerm) => {
+  const response = await fetch(`https://www.howlongtobeat.com/api/search`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Referer': 'https://howlongtobeat.com/',
+    },
+    body: JSON.stringify({
+      "searchType": "games",
+      "searchTerms": [searchTerm],
+      "searchPage": 1,
+      "size": 20,
+      "searchOptions": {
+        "games": {
+          "userId": 0,
+          "platform": "",
+          "sortCategory": "popular",
+          "rangeCategory": "main",
+          "rangeTime": {
+            "min": 0,
+            "max": 0
+          },
+          "gameplay": {
+            "perspective": "",
+            "flow": "",
+            "genre": ""
+          },
+          "modifier": ""
+        },
+        "users": {
+          "sortCategory": "postcount"
+        },
+        "filter": "",
+        "sort": 0,
+        "randomizer": 0
+      }
+    })
+  })
+  return await response.json();
+}
