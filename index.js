9137b5eefe1b498d7e7e3c64bd1b218d62f17e6e
Ceryl Bruitzman <ceryl.bruitzman@topicus.nl>
2023-11-21 13:27:09 +0100

    503 error for when NSG reviews are unavailable

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.get('/nsg-reviews-status', (request, response) => {
     if( result.status === 200) {
       response.status(200).send(result);
     } else if (result.status === 503) {
-      response.status(500).send({ error: 'NSG Reviews is unavailable' });
+      response.status(503).send({ error: 'NSG Reviews is unavailable' });
     } else {
       response.status(500).send({ error: 'Someting went wrong' });
     }
